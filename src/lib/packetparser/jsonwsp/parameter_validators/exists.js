d9df1e37137eff267476a45d1bfa0aa4a6c0c294
Hendrik Janiesch <hendrik.janiesch@gmail.com>
2015-12-04 14:17:50 +0100

    wrote parameter_validator

---
+++ src/lib/packetparser/jsonwsp/parameter_validators/exists.js
@@ -0,0 +1,20 @@
+/**
+ * Created by hendrik on 04.12.2015.
+ */
+'use strict';
+
+function validateParameter(_args,_argName){
+    "use strict";
+
+    if(_args.hasOwnProperty(_argName)){
+        if(_args[_argName] != undefined){
+            return true;
+        }
+    }
+
+    return false;
+}
+
+module.exports = {
+    validateParameter
+};
