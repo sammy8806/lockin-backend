2ba83609a92262777d17bdef56482a4b30e8bffd
Hendrik Janiesch <hendrik.janiesch@gmail.com>
2015-12-04 21:28:00 +0100

    Created jsonwsp_response class
    also added a testcase for jsonwsp_response

--- src/lib/packetparser/jsonwsp/tests/jsonwsp_protocol_test.js
+++ src/lib/packetparser/jsonwsp/tests/jsonwsp_protocol_test.js
@@ -12,3 +12,14 @@ let errData = {
 };
 let fault1 = new JsonWspFault(errData,mirrorData);
 console.log(fault1.toJson());
+
+let JsonWspResponse = require('../protocol/jsonwsp_response.js');
+
+let servicename = 'userservice';
+let methodname = 'register';
+let result = {
+    'success' : true
+};
+mirrorData = 'c00lM1Rr0rData';
+let response1 = new JsonWspResponse(servicename,methodname,result,mirrorData);
+console.log(response1.toJson());
