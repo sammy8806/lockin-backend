f1d49ecb351ede80e33d86a05ca5ec00bceeaeb9
Steven Tappert <admin@dark-it.net>
2015-12-08 23:30:41 +0100

    fixed echo-server updated cl-node.service

--- src/server.js
+++ src/server.js
@@ -1,14 +1,13 @@
 'use strict';
-
 const SERVER_PORT = 8080;
 let WebSocketServer = require('ws').Server;
 
 let websock = new WebSocketServer({port: SERVER_PORT, host: '::'});
 websock.on('connection', function (ws) {
-    debug('new connection');
+    console.info('new connection');
 
     ws.on('message', function (_msg) {
-        debug(`-> ${_msg}`);
+        console.info(`-> ${_msg}`);
         ws.send(_msg);
     });
 });
