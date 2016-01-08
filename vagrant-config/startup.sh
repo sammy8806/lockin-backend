d869553d3fa3dcf08e605382c340a6ed49fcfff2
Steven <admin@dark-it.net>
2016-01-08 11:44:44 +0100

    added gulp-compile for starting

--- vagrant-config/startup.sh
+++ vagrant-config/startup.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 echo "Checking NPM Stuff"
-PATH=/usr/local/bin:/usr/bin:/bin
+PATH=/usr/local/bin:/usr/bin:/bin:/srv/server/node_modules/.bin
 
 if [ -f "/srv/server/.vars.local" ]; then
     . /srv/server/.vars.local
 fi
 
-export SERVER_DEVEL=1
+# export SERVER_DEVEL=1
 
 if [ -z "$SERVER_DEVEL" ]; then
     cd /srv/server/
@@ -18,4 +18,9 @@ if [ -z "$SERVER_DEVEL" ]; then
     npm update
 
     echo "npm done ..."
+    
+    echo "Running Gulp"
+    rm -rf /srv/server/dist
+    gulp compile
+    
 fi
