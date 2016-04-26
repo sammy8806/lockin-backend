72166571ac71591c1d89388c58354717834bed55
Jan <jmueller02@stud.hs-bremen.de>
2016-04-26 11:42:32 +0200

    moved config file to /lib

--- src/lib/config.js
+++ src/lib/config.js
@@ -4,7 +4,7 @@ module.exports = {
         port: 8090
     },
     database: {
-        host: 'mongodb://cl2-mongo', 
+        host: 'mongodb://cl2-mongo',
         port: '27017',
         dbname: 'contentloops'
     }
