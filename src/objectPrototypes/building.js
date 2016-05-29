4311bd6b6dbee4e9800232c7cdf251aca8489e0a
Steven Tappert <admin@dark-it.net>
2016-05-29 21:12:33 +0200

    allow building-name

--- src/objectPrototypes/building.js
+++ src/objectPrototypes/building.js
@@ -6,6 +6,7 @@ module.exports = class Building extends ObjectPrototype {
     constructor(_building) {
         const _whitelistedAttributes = [
             'id',
+            'name',
             'keyId', //KeyId of creator
             'street',
             'houseNumber',
